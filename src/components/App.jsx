dde768bc11dcbca09d225a534292a678b6440cf1
axewert <secret4talk@gmail.com>
2021-02-03 14:28:03 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,41 +7,29 @@ function App({currentTabs, tabs}) {
 
   return (
     <>
-    <div className="new-tab-form">
+    
       
-    </div>
-    <div >
       <div className="tab-container">
         <input type="radio" className="tab-check" name="btnradio" id="btnradio1" autoComplete="off" />
         <label className="tab-item" htmlFor="btnradio1">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio2" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio2">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <input type="radio" className="tab-check" name="btnradio" id="btnradio3" autoComplete="off" />
-        <label className="tab-item" htmlFor="btnradio3">Radio 1</label>
-        <div className="new-tab-input">
+        
+        <div className="input-container new-tab-input">
           <input className='input' type="text" placeholder="Add new Tab"/>
-          {/* <button className="btn new-tab-btn tab-item">add new tab</button> */}
         </div>
        
       </div>
-    </div>
 
     <div className="group-container">
       <div className="group-item">
         <h3 className="group-item-header">Group1</h3>
         <ul className="tasks-group">
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
+          <li className="task-group-item">Cras justo odio</li>
           <li className="task-group-item">Cras justo odio</li>
         </ul>
 
